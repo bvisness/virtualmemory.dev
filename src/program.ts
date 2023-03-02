281ad1548ffe3b2dd1e56004f24923e1e807f75e
Ben Visness <ben@bvisness.me>
2023-03-02 16:44:36 -0600

    Implement memset or whatever

--- src/program.ts
+++ src/program.ts
@@ -1,11 +1,52 @@
 // Types for the user program
 
-import { WinAllocationType, WinFreeType, WinProtect } from "./windows";
-
 interface Memset {
+    type: "memset";
     base: number;
     value: number;
     length: number;
+}
+
+interface WinReservation {
+    base: number;
+    length: number;
+}
+
+enum WinPageState {
+    Free = "free",
+    Reserved = "reserved",
+    Committed = "committed",
+}
+
+enum WinAllocationType {
+    MEM_COMMIT = 0x00001000,
+    MEM_RESERVE = 0x00002000,
+    MEM_RESET = 0x00080000,
+    MEM_RESET_UNDO = 0x1000000,
+}
+
+enum WinProtect {
+    PAGE_EXECUTE = 0x10,
+    PAGE_EXECUTE_READ = 0x20,
+    PAGE_EXECUTE_READWRITE = 0x40,
+    PAGE_EXECUTE_WRITECOPY = 0x80,
+    PAGE_NOACCESS = 0x01,
+    PAGE_READONLY = 0x02,
+    PAGE_READWRITE = 0x04,
+    PAGE_WRITECOPY = 0x08,
+
+    PAGE_GUARD = 0x100,
+    PAGE_NOCACHE = 0x200,
+    PAGE_WRITECOMBINE = 0x400,
+}
+
+enum WinFreeType {
+    MEM_DECOMMIT = 0x00004000,
+    MEM_RELEASE = 0x00008000,
+
+    // Modifiers for MEM_RELEASE when placeholders are used
+    MEM_COALESCE_PLACEHOLDERS = 0x00000001,
+    MEM_PRESERVE_PLACEHOLDER = 0x00000002,
 }
 
 /**
@@ -15,6 +56,7 @@ interface Memset {
 type CommonCall = Memset;
 
 interface WinVirtualAlloc {
+    type: "VirtualAlloc";
     lpAddress: number;
     dwSize: number;
     flAllocationType: WinAllocationType;
@@ -22,6 +64,7 @@ interface WinVirtualAlloc {
 }
 
 interface WinVirtualFree {
+    type: "VirtualFree";
     lpAddress: number;
     dwSize: number;
     dwFreeType: WinFreeType;
@@ -29,7 +72,78 @@ interface WinVirtualFree {
 
 type WinCall = CommonCall | WinVirtualAlloc | WinVirtualFree;
 
+interface WinPage {
+    state: WinPageState;
+    protection: WinProtect;
+}
+
+const defaultWinPage: WinPage = {
+    state: WinPageState.Free,
+    protection: WinProtect.PAGE_NOACCESS,
+};
+
+class WinMemory {
+    mem: number[];
+    pageSize: number;
+    reservations: WinReservation[];
+    pages: Map<number, WinPage>; // indexed by base address
+
+    constructor(numPages: number) {
+        const pageSize = 4096; // hardcoded for now
+        this.mem = Array<number>(pageSize * numPages).fill(0),
+        this.pageSize = pageSize;
+        this.reservations = [];
+        this.pages = new Map();
+    }
+
+    pageForAddr(addr: number): WinPage {
+        const pageBase = Math.floor(addr / this.pageSize) * this.pageSize;
+        return this.pages.get(pageBase) ?? defaultWinPage;
+    }
+}
+
 interface Instruction {
     winCall?: WinCall;
     // TODO: Mac and Linux calls.
+}
+
+interface Program {
+    instrs: Instruction[];
+}
+
+function assertUnreachable(_: never): never {
+    throw new Error("This was supposed to be unreachable >:(");
+}
+
+function runProgramWin(p: Program) {
+    const m = new WinMemory(16);
+
+    for (const inst of p.instrs) {
+        const call = inst.winCall;
+        if (!call) {
+            throw new Error("instruction doesn't work on Windows");
+        }
+
+        switch (call.type) {
+            case "VirtualAlloc": {
+                console.log("u haz");
+            } break;
+            case "VirtualFree": {
+                console.log("freedom!!");
+            } break;
+            case "memset": {
+                for (let i = 0; i < call.length; i++) {
+                    const addr = call.base + i;
+                    const page = m.pageForAddr(addr);
+                    if (page.state !== WinPageState.Committed) {
+                        throw new Error(`SEGFAULT: page for address ${addr} was not committed`);
+                    }
+                    m.mem[addr] = call.value % 256;
+                }
+            } break;
+            default: {
+                assertUnreachable(call);
+            }
+        }
+    }
 }
